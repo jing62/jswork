eb84cc047c0877ab29b472754d3b5d516f276781
jing62 <55219773+jing62@users.noreply.github.com>
2019-10-16 20:38:48 +0800

    Update work5.js

--- work5/work5.js
+++ work5/work5.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //9X9的空表，请自行编写这段代码
 str = '<table border="1">'
 for(let i=9;i>0;--i){
@@ -33,40 +32,4 @@ for(let i=9;i>0;--i){
 str +='</tr>'
 }
 str +='</table>'
-=======
-//9X9的空表，请自行编写这段代码
-str = '<table border="1">'
-for(let i=9;i>0;--i){
-    str += '<tr>'
-    for(var j=9;j>0;--j){
-    str += '<td>&nbsp;</td>'
-}
-str +='</tr>'
-}
-str +='</table>'
-document.getElementById('table1').innerHTML = str
-
-//倒九九乘法空表
-str = '<table border="1">'
-for(let i=9;i>0;--i){
-    str += '<tr>'
-    for(var j=i;j>0;--j){
-    str += '<td>&nbsp;</td>'
-}
-str +='</tr>'
-}
-str +='</table>'
-document.getElementById('table2').innerHTML = str
-
-//倒九九乘法表，请自行编写这段代码
-str = '<table border="1">'
-for(let i=9;i>0;--i){
-    str += '<tr>'
-    for(var j=i;j>0;--j){
-    str += '<td>' + j + '*' +i + (j*i) + '</td>'
-}
-str +='</tr>'
-}
-str +='</table>'
->>>>>>> 1a428f2e1ba2f6ae7a1cd85197c82fbf50d99d8d
 document.getElementById('table3').innerHTML = str
